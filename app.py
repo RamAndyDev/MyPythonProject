3595f9ff8f9cbaaeb3046db6b89d351eb9fd54ed
Ramprasad G <mail2ram.andydev@gmail.com>
2023-12-15 18:34:31 +0530

    After enter data in register redirecting to success page.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
+# from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -12,12 +13,15 @@ def index():
 def register():
     username = request.form["username"]
     email = request.form["email"]
-    password = request.form["password"]
 
     # Implement logic to handle user registration
     # (e.g., store user information in a database)
 
     return render_template("success.html", username=username)
+
+    # Save username in session (optional for further use)
+    # session["username"] = username
+    # return redirect("/success?username=" + username)
 
 
 if __name__ == "__main__":
